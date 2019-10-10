f6e0f6d566d5ebb28c9c0a88e55951cd26fcadc7
KS Li <liks@LiksAir.local>
2019-10-10 23:15:10 +0800

    finish submit

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def rtv(PRE,TERM,opt):
             return answer
         elif TERM not in postings:
             return PRE
-        elif PRE == []
+        elif PRE == []:
             return postings[TERM]
         else:
             i = len(PRE)
@@ -47,8 +47,8 @@ def rtv(PRE,TERM,opt):
             x=0
             y=0
             
-            PRE.append(99999999999999999)
-            postings[TERM].append(99999999999999999)
+            PRE.append('99999999999999999')
+            postings[TERM].append('99999999999999999')
 
             while x<i or y<j:
                 if PRE[x]==postings[TERM][y]:
@@ -61,6 +61,7 @@ def rtv(PRE,TERM,opt):
                 else:
                     answer.append(postings[TERM][y])
                     y+=1
+            postings[TERM].pop()
         return answer
 
     elif opt == 3:
@@ -69,15 +70,15 @@ def rtv(PRE,TERM,opt):
             return answer
         elif TERM not in postings:
             return PRE
-        elif PRE == []
+        elif PRE == []:
             return answer
         else:
             i = len(PRE)
             j = len(postings[TERM])
             x=0
             y=0
-            postings[TERM].append(99999999999999999)
-            while x<i and y<j:
+            postings[TERM].append('99999999999999999')
+            while x<i:
                 if PRE[x]==postings[TERM][y]:
                     # answer.append(postings[TERM][x])
                     x+=1
@@ -87,41 +88,52 @@ def rtv(PRE,TERM,opt):
                     x+=1
                 else:
                     # answer.append(postings[TERM][y])
-                    y+=1           
+                    y+=1
+            postings[TERM].pop()  
         return answer
 
-def retrive(string):
-    wordlist = str.split()
-    bool flag
-    bool endbit =false
+def retrive(wordlist):
+    # wordlist = str.split()
+    jump = False
+    endbit = False
+    firstbit = True
     preset = []
+    # cnt = 1
     for i,word in enumerate(wordlist):
-        if flag:
-            flag = false
+        # print("我是第%s次进枚举" % cnt)
+        # cnt += 1
+        if jump==True:
+            jump = False
             # flag用于跳过下次循环
-            continue
+            pass
         if i+1 != len(wordlist):#直到倒数第二个
+            # print(i)
+            # print(len(wordlist))
             if word == "and":
                 preset = rtv(preset,wordlist[i+1],1)
-                endbit = false
-                flag = true
+                endbit = False
+                jump = True
             elif word == "or":
                 preset = rtv(preset,wordlist[i+1],2)
-                endbit = false
-                flag = true
+                endbit = False
+                jump = True
             elif word == "not":
                 preset = rtv(preset,wordlist[i+1],3)
-                endbit = false
-                flag = true
-            else
-                preset = rtv(preset,word,1)
-                endbit = true
-        elif endbit == true:
+                endbit = False
+                jump = True
+            else:
+                # print("第一个是%s" % word)
+                if firstbit == True:
+                    preset = rtv(preset,word,2)
+                    firstbit = False
+                else:
+                    preset = rtv(preset,word,1)
+                endbit = True
+        elif endbit == True:
             preset = rtv(preset,word,1)
     return preset
 
 def tokenize_tweet(string):
-    
     string=string.lower()
     a = string.index("username")
     b = string.index("clusterno")
@@ -133,7 +145,7 @@ def tokenize_tweet(string):
     string = string[c:d]+string[a:b]+string[e:f]
     # print(string)
     terms=TextBlob(string).words.singularize()
-    print(terms)
+    # print(terms)
 
     result=[]
     for word in terms:
@@ -143,7 +155,7 @@ def tokenize_tweet(string):
             result.append(expected_str)
     return result
 
-def token(query):#做标准化
+def token(query):#做normalize
     query = query.lower()
     terms=TextBlob(query).words.singularize()
 
@@ -154,10 +166,10 @@ def token(query):#做标准化
         result.append(expected_str)
     return result
 
-def get_postings():
+def GeneratePosting():
 
     global postings
-    f = open(r"/Users/liks/iGit/irGit/data/demo.txt")  
+    f = open(r"data/tweets.txt")  
     lines = f.readlines()#读取全部内容
 
     for line in lines:
@@ -170,30 +182,23 @@ def get_postings():
                postings[te].append(tweetid)
            else:
                postings[te] = [tweetid]
-    for value in postings.value()
+    for value in postings.values():
         value.sort()
     # print(postings)
 
-def do_search():
+def Search():
     terms = token(input("Search query >> "))
     if terms == []:
         sys.exit()
-
+    # print(terms)
+    print(retrive(terms))
 
 
 def main():
-    get_postings()
+    GeneratePosting()
+    while True:
+        Search()
+
 
 if __name__ == "__main__":
-    main()
-
-
-
-
-
-
-
-
-
-
-    
+    main()    
